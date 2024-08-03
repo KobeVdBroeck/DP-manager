61676cd62ea17b7a2cc7b0243b69bad5841b0773
Kobe Van den Broeck <kobe.vandenbroeck@student.hogent.be>
2024-08-03 17:40:47 +0200

    Test gRPC connection

--- DP manager GUI/Form1.Designer.cs
+++ DP manager GUI/Form1.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 136);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(545, 194);
+            this.textBox1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(853, 487);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
---
+++ DP manager GUI/Plant.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace DP_manager
+{
+    public class Plant
+    {
+        [JsonProperty("jaar")]
+        public int Jaar { get; set; }
+
+        [JsonProperty("soortCode")]
+        public string SoortCode { get; set; }
+
+        [JsonProperty("pm")]
+        public UInt16 Pm { get; set; }
+    }
+}
