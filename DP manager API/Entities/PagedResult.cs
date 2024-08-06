7541392233cb23c37a22c68f571308d16d41056e
Kobe Van den Broeck <kobe.vandenbroeck@student.hogent.be>
2024-08-06 17:25:29 +0200

    Implement paging in API

---
+++ DP manager API/Entities/PagedResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DP_manager_API.Entities
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Result { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+        public int TotalCount { get; set; }
+
+        public PagedResult(IEnumerable<T> result, int page, int pageLimit)
+        {
+            Result = result.Skip(pageLimit * (page - 1)).Take(pageLimit);
+            TotalCount = result.Count();
+            PageCount = 1 + (int) Math.Floor((decimal)(TotalCount / pageLimit));
+            CurrentPage = page;
+        }
+    }
+}
--- DP manager API/Models/SortModel.cs
+++ DP manager API/Models/SortModel.cs
@@ -6,29 +6,6 @@ namespace DP_manager_API.Models
     public class SortModel<T>
     {
         public string FieldName { get; set; }
-        public SortDirection Direction { get; set; } = SortDirection.Ascending;
-
-        public IComparer<T> BuildSortFunction()
-        {
-            if (string.IsNullOrWhiteSpace(FieldName))
-                return Comparer<T>.Create((x, y) => 0);
-
-            Func<T, object> getValue = (t) =>
-            {
-                PropertyInfo pinfo = typeof(T).GetProperty(FieldName);
-                return pinfo.GetValue(t, null);
-            };
-
-            return Comparer<T>.Create((x, y) =>
-            {
-                var valX = getValue(x);
-                var valY = getValue(y);
-
-                if(valX is int && valY is int)
-                    return (int)valY - (int)valX;
-
-                return valX.ToString().CompareTo(valY);
-            });
-        }
+        public string Direction { get; set; } = "asc";
     }
 }
