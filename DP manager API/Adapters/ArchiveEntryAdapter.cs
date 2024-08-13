d961fddf5111889778558ac3f217d5f744569f92
Kobe Van den Broeck <kobe.vandenbroeck@student.hogent.be>
2024-08-13 16:41:14 +0200

    Add first mutation query

---
+++ DP manager API/Adapters/ArchiveEntryAdapter.cs
@@ -0,0 +1,30 @@
+﻿using DP_manager_API.Entities;
+
+namespace DP_manager_API.Adapters
+{
+    public static class ArchiveEntryAdapter
+    {
+        public static ArchiveEntry Adapt(this StockEntry stock, string reason = "No reason specified")
+        {
+            return new ArchiveEntry()
+            {
+                Category = stock.Category,
+                Health = stock.Health,
+                History = stock.History + stock.Id + ";",
+                Lab = stock.Lab,
+                Location = stock.Location,
+                Medium = stock.Medium,
+                MediumId = stock.MediumId,
+                Phase = stock.Phase,
+                Plant = stock.Plant,
+                PlantCode = stock.PlantCode,
+                Ppr = stock.Ppr,
+                Recipients = stock.Recipients,
+                Remarks = stock.Remarks,
+                Week = stock.Week,
+                Worker = stock.Worker,
+                Reason = reason
+            };
+        }
+    }
+}
---
+++ DP manager API/Adapters/StockEntryAdapter.cs
@@ -0,0 +1,29 @@
+﻿using DP_manager_API.Entities;
+
+namespace DP_manager_API.Adapters
+{
+    public static class StockEntryAdapter
+    {
+        public static StockEntry WithoutId(this StockEntry stock, string history)
+        {
+            return new StockEntry()
+            {
+                Category = stock.Category,
+                Health = stock.Health,
+                History = history,
+                Lab = stock.Lab,
+                Location = stock.Location,
+                Medium = stock.Medium,
+                MediumId = stock.MediumId,
+                Phase = stock.Phase,
+                Plant = stock.Plant,
+                PlantCode = stock.PlantCode,
+                Ppr = stock.Ppr,
+                Recipients = stock.Recipients,
+                Remarks = stock.Remarks,
+                Week = stock.Week,
+                Worker = stock.Worker
+            };
+        }
+    }
+}
